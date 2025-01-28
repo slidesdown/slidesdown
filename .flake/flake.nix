fa31bf1935812995cc964bd432f079f650fb4415
Jan Christoph Ebersbach <jceb@e-jc.de>
2025-01-28 13:30:43 +0100

    chore(flake): update configuration

--- .flake/flake.nix
+++ .flake/flake.nix
@@ -2,18 +2,24 @@
 {
   description = "Dependencies";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   inputs.nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, nixpkgs_unstable, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs_unstable,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         unstable = nixpkgs_unstable.legacyPackages.${system};
-        my-python-packages = python-packages:
-          with python-packages;
-          [
+        my-python-packages =
+          python-packages: with python-packages; [
             # pyyaml # YAML module
           ];
         python-with-my-packages = pkgs.python3.withPackages my-python-packages;
@@ -25,8 +31,8 @@
           gh # GitHub CLI https://cli.github.com/
           git-cliff # Changelog generator https://github.com/orhun/git-cliff
           just # Simple make replacement https://just.systems/
-          nodejs_20 # node used for husky installation https://nodejs.org/en/
-          unstable.nushell # Nu Shell https://www.nushell.sh/
+          nodejs_22 # node used for husky installation https://nodejs.org/en/
+          nushell # Nu Shell https://www.nushell.sh/
           python-with-my-packages
         ];
         linuxOnlyPackages = [ ];
@@ -34,12 +40,10 @@
       {
         devShell = pkgs.mkShell {
           nativeBuildInputs =
-            if pkgs.system == "x86_64-linux" then
-              allOsPackages ++ linuxOnlyPackages
-            else
-              allOsPackages;
+            if pkgs.system == "x86_64-linux" then allOsPackages ++ linuxOnlyPackages else allOsPackages;
           buildInputs = [ ];
         };
 
-      });
+      }
+    );
 }
